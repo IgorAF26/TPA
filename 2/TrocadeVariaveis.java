dca2038d75d51007c3eae7898d55a3be53d6845b
Igor Aguiar <167988735+IgorAF26@users.noreply.github.com>
2024-09-20 09:16:22 -0300

    Add files via upload

---
+++ 2/TrocadeVariaveis.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class TrocadeVariaveis {
+	
+	public static void main(String[] args) {
+		int a, b, c;
+		Scanner Ler = new Scanner(System.in);
+		
+		System.out.println("Insira o valor a:");
+		a = Ler.nextInt();
+		
+		System.out.println("Insir o valor b:");
+		b = Ler.nextInt();
+		
+		System.out.println(a + ", " + b );
+		
+		c = a;
+		a = b;
+		b = c;
+		
+		System.out.println(a + ", " + b );
+		
+		Ler.close();
+		
+		
+	}
+}
